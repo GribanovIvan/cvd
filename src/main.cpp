0dfd836686ee74ae835044b54c53d3e6f1542e4e
Hf1b <itzdaniel1337@gmail.com>
2021-02-23 11:54:08 +0500

    skeleton

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char* argv[]) {
+  std::cout << "CVD - CoolVideoDownloader v" << VERSION_MAJOR
+    << "." << VERSION_MINOR << std::endl;
+  return 0;
+}
